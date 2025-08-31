b01d0d342dab5ac27fa7b362ccf32fc5695a6c05
eric-ideas <ericshanghungwu@gmail.com>
2025-08-31 22:24:37 +0000

    Deploying to gh-pages from @ eric-ideas/eric-ideas.github.io@795d237a0638d4642e7e789138197ecca87687b7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "post-linear-algebra-ii-2025-notes",
         
           title: "Linear Algebra II 2025 Notes",
