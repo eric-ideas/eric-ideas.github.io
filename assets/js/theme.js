0c7629f9f3acf0ec580e7fe64dbbaaf7c7d64652
Eric Wu <ericshanghungwu@gmail.com>
2025-08-31 21:28:42 -0400

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -254,11 +254,7 @@ let transTheme = () => {
 // Determine the expected state of the theme toggle, which can be "dark", "light", or
 // "system". Default is "system".
 let determineThemeSetting = () => {
-  let themeSetting = localStorage.getItem("theme");
-  if (themeSetting != "dark" && themeSetting != "light" && themeSetting != "system") {
-    themeSetting = "system";
-  }
-  return themeSetting;
+  return "dark";
 };
 
 // Determine the computed theme, which can be "dark" or "light". If the theme setting is
