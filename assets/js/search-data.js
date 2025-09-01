19b243b20bd0a002615d280c3ea47961259db91e
eric-ideas <ericshanghungwu@gmail.com>
2025-09-01 00:18:32 +0000

    Deploying to gh-pages from @ eric-ideas/eric-ideas.github.io@236cdb877b0dad9d69039667169b579e039532ab 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -65,62 +65,14 @@ ninja.data = [{
           description: "",
           section: "Gallery",handler: () => {
               window.location.href = "/gallery/example-g/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-i-am-currently-taking-mged11-stab52-matb41-matb44-stab40-cscc37-in-order-theory-and-practice-of-regression-analysis-an-introduction-to-probability-mathematical-treatment-techniques-of-the-calculus-of-several-variables-i-differential-equation-i-fundemental-of-investment-and-credit-and-introduction-to-numerical-methods-see-my-blog-named-introspection-for-my-academic-journal-i-update-what-i-learned-and-some-of-my-remarks-or-thoughts-each-week",
+          title: 'I am currently taking MGED11, STAB52, MATB41, MATB44, STAB40, CSCC37. In order: Theory...',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "projects-intermediate-microeconomics",
+          title: 'Intermediate Microeconomics',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/empty/";
             },},{id: "teaching-intermediate-microeconomics",
           title: 'Intermediate Microeconomics',
           description: "",
